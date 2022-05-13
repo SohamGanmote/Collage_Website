f0fb6125608cb887f6800ace9b2a67dac473eff2
SohamGanmote <77290684+SohamGanmote@users.noreply.github.com>
2022-05-13 21:20:24 +0530

    Delete javascript directory

--- javascript/popup.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("profile").addEventListener("click", function () {
-  document.querySelector(".popup").style.display = "flex";
-  document.querySelector(".master-container").style.opacity = 0.5
-})
-
-document.getElementById("close").addEventListener("click", function () {
-  document.querySelector(".popup").style.display = "none";
-  document.querySelector(".master-container").style.opacity = 1
-})
